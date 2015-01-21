3014fb839f75be18b157201c442daa412ea82ceb
adamberman <adamberman13@gmail.com>
2015-01-21 13:33:36 -0800

    create comments model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,7 @@
 class Comment < ActiveRecord::Base
+	validates :content, :commentable, presence: true
+
+	has_many :comments, as: :commentable, dependent: :destroy
+
+	belongs_to :commentable, polymorphic: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
 	validates :author, :title, :content, presence: true
 
 	has_many :images
+	has_many :comments, as: :commentable, dependent: :destroy
 
 	belongs_to :author, class_name: 'User'
 end
