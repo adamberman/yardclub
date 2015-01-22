810be510d4b92b687cbfd376d585c968dc5df40c
adamberman <adamberman13@gmail.com>
2015-01-22 12:05:25 -0800

    write methods for comments controller

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,4 +1,40 @@
 module Api
 	class CommentsController < ApplicationController
+		def index
+			@post = Post.find(params[:post_id])
+			@comments = @post.comments
+			render :index
+		end
+
+		def create
+			@comment = Comment.new(comment_params)
+
+			if @comment.save
+				render :show, status: :created
+			else
+				render json: @comment.errors.full_messages, status: :unprocessable_entity
+			end
+		end
+
+		def destroy
+			# if i were going to make sure that you could only
+			# destroy comments you've created, i would build auth
+			# (which I believe is beyond the scope of this challenge)
+			# and include a current_user method. i would then also include
+			# an author column for comments as well, and only allow 'destroy'
+			# when author == current_user. for examples of auth that i've
+			# built in the past, check out the user model and user/session/application
+			# controllers in my TVBNB repo: github.com/adamberman/tvbnb
+			
+			@comment = Comment.find(params[:id])
+			@comment.destroy
+			render json: {}
+		end
+
+		private
+
+		def comment_params
+			params.require(:comment).permit(:commentable_type, :commentable_id, :content)
+		end
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-    resources :posts, except: [:new, :edit]
+    resources :posts, except: [:new, :edit] do
+    	resources :comments, only: :index
+    end
     resources :images, only: [:create, :destroy]
-    resources :comments
+    resources :comments, only: [:create, :destroy]
   end
 end
